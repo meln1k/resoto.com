676652cdd5ee5e00804c3e029796161e92cd229a
TheCatLady <TheCatLady@users.noreply.github.com>
2022-01-31 00:00:17 +0000

    deploy: edaf5cb655db51d5990415c26b2853de1043d669

--- assets/js/251c4cdf.37b70a22.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkresoto_com=self.webpackChunkresoto_com||[]).push([[6675],{3531:function(e,o,n){n.r(o),n.d(o,{default:function(){return c}});var t=n(7294);function c(){return(0,t.useEffect)((function(){window.location.href="https://resoto.com/docs/reference/core-api/"}),[]),null}}}]);
